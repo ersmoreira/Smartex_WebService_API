47238f6d721b90ec775d89114619fca1861c0f39
Edgar Moreira <1010100@isep.ipp.pt>
2023-06-11 15:03:11 +0100

    Implementation of Challenge API

---
+++ src/main/java/features/employee.feature
@@ -0,0 +1,18 @@
+Feature: Employee API Testing
+
+  Scenario: Create an employee
+    Given I have the employee details
+    When I send a POST request to "/api/v1/create"
+    Then the response status code should be 200
+
+  Scenario: Validate if the employee was created correctly
+    Given I have the employee ID
+    When I send a GET request to "/api/v1/employee/{id}"
+    Then the response status code should be 200
+    And the employee details should match the created employee
+
+  Scenario: Delete the employee
+    Given I have the employee ID
+    When I send a DELETE request to "/api/v1/delete/{id}"
+    Then the response status code should be 200
+    And the employee should be deleted successfully
---
+++ src/test/java/stepdefinitions/EmployeeSteps.java
@@ -0,0 +1,78 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Assert;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EmployeeSteps {
+    private Response response;
+    private int employeeId;
+    private Map<String, String> employeeDetails;
+
+    @Given("I have the employee details")
+    public void iHaveTheEmployeeDetails() {
+        employeeDetails = new HashMap<>();
+        employeeDetails.put("name", "Edgar Moreira");
+        employeeDetails.put("salary", "2500");
+        employeeDetails.put("age", "42");
+    }
+
+    @When("I send a POST request to {string}")
+    public void iSendPOSTRequest(String endpoint) {
+        RequestSpecification request = RestAssured.given()
+                                                  .contentType(ContentType.JSON)
+                                                  .body(employeeDetails);
+
+        response = request.post(endpoint);
+    }
+
+    @Then("the response status code should be {int}")
+    public void theResponseStatusCodeShouldBe(int expectedStatusCode) {
+        Assert.assertEquals(expectedStatusCode, response.getStatusCode());
+    }
+
+    @Then("the employee should be created successfully")
+    public void theEmployeeShouldBeCreatedSuccessfully() {
+        String responseBody = response.getBody().asString();
+        Assert.assertTrue(responseBody.contains("\"status\":\"success\""));
+        employeeId = response.jsonPath().getInt("data.id");
+    }
+
+    @Given("I have the employee ID")
+    public void iHaveTheEmployeeID() {
+        Assert.assertNotNull(employeeId);
+    }
+
+    @When("I send a GET request to {string}")
+    public void iSendGETRequest(String endpoint) {
+        response = RestAssured.get(endpoint.replace("{id}", String.valueOf(employeeId)));
+    }
+
+    @Then("the employee details should match the created employee")
+    public void theEmployeeDetailsShouldMatchTheCreatedEmployee() {
+        String responseBody = response.getBody().asString();
+        Assert.assertTrue(responseBody.contains("\"id\":" + employeeId));
+        Assert.assertTrue(responseBody.contains("\"employee_name\":\"Edgar Moreira\""));
+        Assert.assertTrue(responseBody.contains("\"employee_salary\":\"2500\""));
+        Assert.assertTrue(responseBody.contains("\"employee_age\":\"42\""));
+    }
+
+    @When("I send a DELETE request to {string}")
+    public void iSendDELETERequest(String endpoint) {
+        response = RestAssured.delete(endpoint.replace("{id}", String.valueOf(employeeId)));
+    }
+
+    @Then("the employee should be deleted successfully")
+    public void theEmployeeShouldBeDeletedSuccessfully() {
+        String responseBody = response.getBody().asString();
+        Assert.assertTrue(responseBody.contains("\"status\":\"success\""));
+    }
+}
